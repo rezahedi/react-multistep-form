7171f4e6166950335d94217aac00adb3b1379618
Reza Zahedi <r.zahedi@gmail.com>
2024-02-01 14:22:11 -0800

    useMultistepForm hook used

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import useMultistepForm from './useMultistepForm';
 
 function App() {
+  const {steps, currentStepIndex, step} = useMultistepForm([
+    <div>One</div>,
+    <div>Two</div>,
+    <div>Three</div>,
+  ]);
 
   return (
     <div className='container'>
       <main>
-        <div className='step'>1 / 2</div>
+        <div className='step'>{currentStepIndex + 1} / {steps.length}</div>
         <form>
-          form
+          {step}
         </form>
       </main>
     </div>
